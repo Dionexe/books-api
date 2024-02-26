186c1a31193d62f15a4ea129d435fe94a35eb4dd
Dion Wells <dionwells@Dions-MacBook-Pro.local>
2024-02-26 15:25:33 -0800

    Completed Assignment

---
+++ models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const {Schema} = mongoose;
+
+//Schema
+const booksSchema = new Schema({
+    title: String,
+    description: String,
+    year: Number,
+    quantity: Number,
+    imageURL: String
+});
+
+const Books = mongoose.model('Books', booksSchema );
+
+module.exports = Books;
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // DEPENDENCIES
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -9,13 +10,21 @@ const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
+const corsOptions = {
+  optionsSuccessStatus: 200
+}
 
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
+app.use(cors())
 
 // ROUTES
 app.get('/', (req, res) => {
-  res.send('Welcome to the Hello World! API')
+  res.send('Welcome to the Hello World!')
+})
+
+app.get('/books/:id', cors(corsOptions), function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled'})
 })
 
 // LISTEN
